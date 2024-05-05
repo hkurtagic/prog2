1466411589d3289b36f254d43fd7ab2254997bcc
Haris <haris.kurtagic@stud.fh-campuswien.ac.at>
2024-05-06 01:51:54 +0200

    Changed width

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -1,18 +1,21 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.database.DatabaseManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class FhmdbApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FhmdbApplication.class.getResource("/at/ac/fhcampuswien/fhmdb/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 890, 620);
+        Scene scene = new Scene(fxmlLoader.load(), 930, 620);
         scene.getStylesheets().add(Objects.requireNonNull(FhmdbApplication.class.getResource("styles.css")).toExternalForm());
         stage.setTitle("FHMDb");
         stage.setScene(scene);
